99f99643583bf55e417400dbdf35dd3042cde417
LJ-debug <60039526+LJ-debug@users.noreply.github.com>
2024-02-13 21:58:57 +0800

    update

--- ClientSide/src/pages/CartUser.jsx
+++ ClientSide/src/pages/CartUser.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useContext, navigate } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import {
   Box,
@@ -39,8 +39,6 @@ import http from "../http";
 import dayjs from "dayjs";
 import global from "../global";
 import UserContext from "../contexts/UserContext";
-<<<<<<< Updated upstream
-=======
 import Pagination from "@mui/material/Pagination";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { loadStripe } from "@stripe/stripe-js";
@@ -61,55 +59,55 @@ const theme = createTheme({
     },
   },
 });
->>>>>>> Stashed changes
 
 function CartUser() {
   const navigate = useNavigate();
   const [CartList, setCartList] = useState([]);
-  const [search, setSearch] = useState("");
   const [cart_id, setid] = useState("");
-
   const { user } = useContext(UserContext);
+  const [memberstatus, setMemberStatus] = useState("");
 
   useEffect(() => {
+    http
+      .get(`/Member/${user.userId}`)
+      .then((memberRes) => {
+        const memberstatus = memberRes.data.memberStatus;
+        setMemberStatus(memberstatus);
+        console.log(memberRes.data.memberStatus);
+      })
+      .catch((error) => {
+        console.error("Error fetching member status:", error);
+      });
+
     getCartList();
   }, []);
 
+
+
   const getCartList = () => {
-    http
-      .get(`/Cart/getuser/${user.userId}`)
-      .then((res) => {
-        setCartList(res.data);
-      })
-      .catch(function (err) {
-        toast.error(`${err.response.data.message}`);
-      });
-  };
-
-  const onSearchChange = (e) => {
-    setSearch(e.target.value);
-  };
-  const onSearchKeyDown = (e) => {
-    if (e.key === "Enter") {
-      searchsender();
-    }
-  };
-
-  const onClickSearch = () => {
-    searchsender();
-  };
-
-  const onClickClear = () => {
-    setSearch("");
-    getCartList();
-  };
-
-  const searchsender = () => {
-    if (search.trim() !== "") {
-      http.get(`/Cart?search=${search}`).then((res) => {
-        setCartList(res.data);
-      });
-    }
+    http.get(`/Cart/getuser/${user.userId}`).then((res) => {
+      const cartItems = res.data;
+      console.log(res.data);
+      Promise.all(
+        cartItems.map(
+          (item) =>
+            http.get(`/Event/getEvent/${item.event_ID}`).then((eventRes) => ({
+              ...item,
+              eventName: eventRes.data.Event_Name,
+              eventImage: eventRes.data.imageFile,
+              eventFee: eventRes.data.Event_Fee_Guest,
+              eventuplayFee: eventRes.data.Event_Fee_Uplay,
+              eventntucFee: eventRes.data.Event_Fee_NTUC,
+            })),
+        )
+      )
+        .then((updatedCartItems) => {
+          setCartList(updatedCartItems);
+        })
+        .catch((err) => {
+          toast.error(`${err.response.data.message}`);
+        });
+    });
   };
 
   const [open, setOpen] = useState(false);
@@ -133,29 +131,6 @@ function CartUser() {
     });
   };
 
-<<<<<<< Updated upstream
-  return (
-    <Box>
-      <Typography
-        variant="h5"
-        sx={{ my: 2, color: "black", fontWeight: "bold" }}
-      >
-        Cart Management
-      </Typography>
-      <Box sx={{ display: "flex", alignItems: "center", mb: 2 }}>
-        <Input
-          value={search}
-          placeholder="Search"
-          onChange={onSearchChange}
-          onKeyDown={onSearchKeyDown}
-        />
-        <IconButton color="primary" onClick={onClickSearch}>
-          <Search />
-        </IconButton>
-        <IconButton color="primary" onClick={onClickClear}>
-          <Clear />
-        </IconButton>
-=======
   const handleCheckout = async () => {
     const LineItems = CartList.map((item) => {
       return {
@@ -319,80 +294,8 @@ function CartUser() {
           </DialogActions>
         </Dialog>
         <ToastContainer />
->>>>>>> Stashed changes
       </Box>
-      <Paper sx={{ width: "100%", overflow: "hidden" }}>
-        <TableContainer sx={{ maxHeight: 440 }}>
-          <Table stickyHeader aria-label="sticky table">
-            <TableHead>
-              <TableRow>
-                <TableCell>Cart ID</TableCell>
-                <TableCell>Booking Date</TableCell>
-                <TableCell>Booking Quantity</TableCell>
-                <TableCell>User ID</TableCell>
-                <TableCell>Event ID</TableCell>
-                <TableCell>Voucher ID</TableCell>
-              </TableRow>
-            </TableHead>
-
-            <TableBody>
-              {CartList.sort(
-                (a, b) => new Date(a.UpdatedAt) - new Date(b.UpdatedAt)
-              ).map((data, index) => (
-                <TableRow key={index}>
-                  <TableCell>{data.Cart_ID}</TableCell>
-                  <TableCell>
-                    {dayjs.utc(data.Booking_Date).format(global.datetimeFormat)}
-                  </TableCell>
-                  <TableCell>{data.Booking_Quantity}</TableCell>
-                  <TableCell>{data.userId}</TableCell>
-                  <TableCell>{data.event_ID}</TableCell>
-                  <TableCell>{data.voucher_ID}</TableCell>
-                  <TableCell>
-                    {" "}
-                    <IconButton
-                      color="primary"
-                      onClick={() => handleOpen(data.Cart_ID)}
-                    >
-                      <Clear />
-                    </IconButton>
-                  </TableCell>
-                  <TableCell>
-                    {" "}
-                    <Link to={`/Cart/update/${data.Cart_ID}`}>
-                      <IconButton color="primary" sx={{ padding: "4px" }}>
-                        <Edit />
-                      </IconButton>
-                    </Link>
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </Paper>
-      <Dialog open={open} onClose={handleClose}>
-        <DialogTitle>Delete Event</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            Are you sure you want to Delete this Cart?
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button variant="contained" color="inherit" onClick={handleClose}>
-            Cancel
-          </Button>
-          <Button
-            variant="contained"
-            color="error"
-            onClick={() => deleteCart(cart_id)}
-          >
-            Delete
-          </Button>
-        </DialogActions>
-      </Dialog>
-      <ToastContainer />{" "}
-    </Box>
+    </ThemeProvider>
   );
 }
 
